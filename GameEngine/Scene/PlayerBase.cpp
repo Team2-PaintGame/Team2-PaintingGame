69274397fd71c973d4c91edae26b58156b5e4a33
Aiman Khan <c2055955@newcastle.ac.uk>
2023-01-25 23:42:09 +0000

    added source file for base player

---
+++ GameEngine/Scene/PlayerBase.cpp
@@ -0,0 +1,24 @@
+#include "PlayerBase.h"
+#include "RenderObject.h"
+#include "PhysicsObject.h"
+
+using namespace NCL;
+
+PlayerBase::PlayerBase(Vector3 position, MeshGeometry* mesh, TextureBase* texture, ShaderBase* shader, int size) {
+	name = "BasePlayer";
+	transform
+		.SetScale(Vector3(size, size, size))
+		.SetPosition(position);
+
+	renderObject = new RenderObject(&transform, mesh, shader);
+	renderObject->AddTexture(0, texture, "mainTex");
+	physicsObject = new PhysicsObject(&transform, boundingVolume);
+	//in case of material
+	/*int meshLayers = mesh->GetSubMeshCount();
+	for (int i = 0; i < meshLayers; i++) {
+		renderObject->AddTexture(i, material->GetMaterialForLayer(i)->GetEntry("Diffuse"), "mainTex");
+	}*/
+}
+
+
+
--- GameEngine/Scene/PlayerBase.h
+++ GameEngine/Scene/PlayerBase.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "GameObject.h"
+#include "MeshGeometry.h"
+#include "TextureBase.h"
+#include "ShaderBase.h"
 
 namespace NCL {
-	
+	using namespace Rendering;
 	using namespace CSC8508;
 	class PlayerBase : public GameObject {
 		PlayerBase() = default;
+		PlayerBase(Vector3 position, MeshGeometry* mesh, TextureBase* texture, ShaderBase* shader, int size);
 		virtual void Update(float dt) {}
 		virtual ~PlayerBase() {}
 	};
