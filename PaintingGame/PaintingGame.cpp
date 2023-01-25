4700949f77701fd0730294fd3daf1ea59fcaf354
Aiman Khan <c2055955@newcastle.ac.uk>
2023-01-25 23:48:58 +0000

    deleted unused code from painting game

--- PaintingGame/PaintingGame.cpp
+++ PaintingGame/PaintingGame.cpp
@@ -14,12 +14,10 @@ PaintingGame::PaintingGame() {
 	physics = new PhysicsSystem(*world);
 
 	forceMagnitude = 10.0f;
-	inSelectionMode = false;
 
 	InitialiseAssets();
 	physics->UseGravity(useGravity);
 	renderer->UseFog(useFog);
-	hedgeMaze = nullptr;
 }
 
 /*
@@ -86,7 +84,6 @@ PaintingGame::~PaintingGame() {
 	for (const auto& [key, val] : meshAnimations) {
 		delete val;
 	}
-	delete hedgeMaze;
 	delete physics;
 	delete renderer;
 	delete world;
@@ -110,282 +107,17 @@ void PaintingGame::InitCamera() {
 	world->GetMainCamera()->SetBasicCameraParameters(-15.0f, 315.0f, Vector3(-60, 40, 60), 0.1f, 500.0f);
 	world->GetMainCamera()->SetFirstPersonCamera(); 
 	world->GetMainCamera()->SetPerspectiveCameraParameters(Window::GetWindow()->GetScreenAspect());
-	lockedObject = nullptr;
 }
 
 void PaintingGame::InitWorld() {
 	world->ClearAndErase();
 	physics->Clear();
-	if (!hedgeMaze) delete hedgeMaze;
 
-	//InitMixedGridWorld(1, 1, 3.5f, 3.5f);
 	remainingTime = 2 * 60;
 
 	TerrainTexturePack terrainTexturePack(textures.at("terrainSplatMap"), textures.at("terrainRTex"), textures.at("terrainGTex"), textures.at("terrainBTex"), textures.at("terrainBgTex"));
 	world->AddGameObject(new Terrain(Vector2(), meshes.at("terrainMesh"), terrainTexturePack, shaders.at("terrainShader")));
 	world->AddGameObject(new Terrain(Vector2(0, 1), meshes.at("terrainMesh"), terrainTexturePack, shaders.at("terrainShader")));
-
-	//InitGameExamples();
-	//InitDefaultFloor();
-	//testStateObject = AddStateObjectToWorld(Vector3(15, 10, 0));
-	//AddNavigationGrid();
-}
-
-/*
-
-A single function to add a large immoveable cube to the bottom of our world
-
-*/
-GameObject* PaintingGame::AddFloorToWorld(const Vector3& position) {
-	GameObject* floor = new GameObject("Floor");
-
-	Vector3 floorSize = Vector3(200, 2, 200);
-	AABBVolume* volume = new AABBVolume(floorSize);
-	//OBBVolume* volume = new OBBVolume(floorSize);
-	floor->SetBoundingVolume((CollisionVolume*)volume);
-	floor->GetTransform()
-		.SetScale(floorSize * 2)
-		.SetPosition(position);
-
-	floor->SetRenderObject(new RenderObject(&floor->GetTransform(), meshes.at("cubeMesh"), shaders.at("basicShader")));
-	int numSubMeshes = meshes.at("cubeMesh")->GetSubMeshCount();
-	for (int subMeshIndex = 0; subMeshIndex < numSubMeshes; ++subMeshIndex) {
-		floor->GetRenderObject()->AddTexture(subMeshIndex, textures.at("floorTex"), "mainTex");
-	}
-	//floor->GetRenderObject()->SetColour(Vector4(0, 1, 0, 1));
-	floor->SetPhysicsObject(new PhysicsObject(&floor->GetTransform(), floor->GetBoundingVolume()));
-
-	floor->GetPhysicsObject()->SetInverseMass(0);
-	floor->GetPhysicsObject()->InitCubeInertia();
-	floor->SetLayer(Layer::StaticObjects);
-
-	world->AddGameObject(floor);
-
-	return floor;
-}
-
-/*
-
-Builds a game object that uses a sphere mesh for its graphics, and a bounding sphere for its
-rigid body representation. This and the cube function will let you build a lot of 'simple'
-physics worlds. You'll probably need another function for the creation of OBB cubes too.
-
-*/
-GameObject* PaintingGame::AddSphereToWorld(const Vector3& position, float radius, float inverseMass) {
-	GameObject* sphere = new GameObject("Sphere");
-
-	Vector3 sphereSize = Vector3(radius, radius, radius);
-	SphereVolume* volume = new SphereVolume(radius);
-	sphere->SetBoundingVolume((CollisionVolume*)volume);
-
-	sphere->GetTransform()
-		.SetScale(sphereSize)
-		.SetPosition(position);
-
-	sphere->SetRenderObject(new RenderObject(&sphere->GetTransform(), meshes.at("sphereMesh"), shaders.at("basicShader")));
-	sphere->GetRenderObject()->AddTexture(0, textures.at("basicTex"), "mainTex");
-	sphere->SetPhysicsObject(new PhysicsObject(&sphere->GetTransform(), sphere->GetBoundingVolume()));
-
-	sphere->GetPhysicsObject()->SetInverseMass(inverseMass);
-	sphere->GetPhysicsObject()->InitSphereInertia();
-	//sphere->SetLayer(Layer::Spheres);
-
-	world->AddGameObject(sphere);
-
-	return sphere;
-}
-
-GameObject* PaintingGame::AddCubeToWorld(const Vector3& position, Vector3 dimensions, float inverseMass) {
-	GameObject* cube = new GameObject("Cube");
-
-	AABBVolume* volume = new AABBVolume(dimensions);
-	//OBBVolume* volume = new OBBVolume(dimensions);
-	cube->SetBoundingVolume((CollisionVolume*)volume);
-
-	cube->GetTransform()
-		.SetPosition(position)
-		.SetScale(dimensions * 2);
-
-	cube->SetRenderObject(new RenderObject(&cube->GetTransform(), meshes.at("cubeMesh"), shaders.at("basicShader")));
-	cube->GetRenderObject()->AddTexture(0, textures.at("basicTex"), "mainTex");
-	cube->SetPhysicsObject(new PhysicsObject(&cube->GetTransform(), cube->GetBoundingVolume()));
-
-	cube->GetPhysicsObject()->SetInverseMass(inverseMass);
-	cube->GetPhysicsObject()->InitCubeInertia();
-
-	if (rand() % 2) {
-		cube->GetPhysicsObject()->SetElasticity(1.0f);
-		cube->GetRenderObject()->SetColour(Vector4(1.0f, 0.0f, 0.0f, 1.0f));
-	}
-	else {
-		cube->GetPhysicsObject()->SetElasticity(0.2f);
-		cube->GetRenderObject()->SetColour(Vector4(0.0f, 0.0f, 0.0f, 1.0f));
-	}
-	//cube->SetLayer(Layer::Cubes);
-	world->AddGameObject(cube);
-
-	return cube;
-}
-
-//void PaintingGame::AddCoinsToWorld(const Vector3& position) {
-//	world->AddGameObject(new Coin(position, meshes.at("coinMesh"), textures.at("coinTex"), shaders.at("basicShader")));
-//}
-
-
-//StateGameObject* PaintingGame::AddStateObjectToWorld(const Vector3& position) {
-//	float meshSize = 3.0f;
-//	float inverseMass = 0.5f;
-//
-//	StateGameObject* character = new StateGameObject();
-//
-//	AABBVolume* volume = new AABBVolume(Vector3(0.3f, 0.9f, 0.3f) * meshSize);
-//	//CapsuleVolume* volume = new CapsuleVolume(0.9 * meshSize, 0.3 * meshSize);
-//	character->SetBoundingVolume((CollisionVolume*)volume);
-//
-//	character->GetTransform()
-//		.SetScale(Vector3(meshSize, meshSize, meshSize))
-//		.SetPosition(position);
-//
-//	character->SetRenderObject(new RenderObject(&character->GetTransform(), meshes.at("enemyMesh"), nullptr, shaders.at("basicShader")));
-//	character->SetPhysicsObject(new PhysicsObject(&character->GetTransform(), character->GetBoundingVolume()));
-//
-//	character->GetPhysicsObject()->SetInverseMass(inverseMass);
-//	character->GetPhysicsObject()->InitSphereInertia();
-//	character->SetLayer(Layer::OtherObjects);
-//
-//	world->AddGameObject(character);
-//
-//	return character;
-//}
-
-void PaintingGame::InitDefaultFloor() {
-	AddFloorToWorld(Vector3(140, -20, 130));
-}
-
-void PaintingGame::InitGameExamples() {
-	//AddPlayerToWorld(Vector3(15, 5, 0));
-	//AddCoinsToWorld(Vector3(5, -15, 0));
-}
-
-void PaintingGame::InitSphereGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, float radius) {
-	for (int x = 0; x < numCols; ++x) {
-		for (int z = 0; z < numRows; ++z) {
-			Vector3 position = Vector3(x * colSpacing, 10.0f, z * rowSpacing);
-			AddSphereToWorld(position, radius, 1.0f);
-		}
-	}
-	AddFloorToWorld(Vector3(0, -2, 0));
-}
-
-void PaintingGame::InitMixedGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing) {
-	float sphereRadius = 1.0f;
-	Vector3 cubeDims = Vector3(1, 1, 1);
-
-	for (int x = 0; x < numCols; ++x) {
-		for (int z = 0; z < numRows; ++z) {
-			Vector3 position = Vector3(x * colSpacing, 10.0f, z * rowSpacing);
-
-			if (rand() % 2) {
-				AddCubeToWorld(position, cubeDims);
-			}
-			else {
-				AddSphereToWorld(position, sphereRadius);
-			}
-		}
-	}
-}
-
-void PaintingGame::InitCubeGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, const Vector3& cubeDims) {
-	for (int x = 1; x < numCols + 1; ++x) {
-		for (int z = 1; z < numRows + 1; ++z) {
-			Vector3 position = Vector3(x * colSpacing, 10.0f, z * rowSpacing);
-			AddCubeToWorld(position, cubeDims, 1.0f);
-		}
-	}
-}
-
-/*
-Every frame, this code will let you perform a raycast, to see if there's an object
-underneath the cursor, and if so 'select it' into a pointer, so that it can be
-manipulated later. Pressing Q will let you toggle between this behaviour and instead
-letting you move the camera around.
-
-*/
-bool PaintingGame::SelectObject() {
-	if (Window::GetKeyboard()->KeyPressed(KeyboardKeys::Q)) {
-		inSelectionMode = !inSelectionMode;
-		if (inSelectionMode) {
-			Window::GetWindow()->ShowOSPointer(true);
-			Window::GetWindow()->LockMouseToWindow(false);
-		}
-		else {
-			Window::GetWindow()->ShowOSPointer(false);
-			Window::GetWindow()->LockMouseToWindow(true);
-		}
-	}
-	if (inSelectionMode) {
-		Debug::Print("Press Q to change to camera mode!", Vector2(5, 85));
-
-		if (Window::GetMouse()->ButtonDown(NCL::MouseButtons::LEFT)) {
-			if (selectionObject) {	//set colour to deselected;
-				selectionObject->GetRenderObject()->SetColour(Vector4(1, 1, 1, 1));
-				selectionObject = nullptr;
-			}
-
-			Ray ray = CollisionDetection::BuildRayFromMouse(*world->GetMainCamera());
-			RayCollision closestCollision;
-			if (world->Raycast(ray, closestCollision, true, NULL)) {
-				selectionObject = (GameObject*)closestCollision.node;
-				selectionObject->GetRenderObject()->SetColour(Vector4(0, 1, 0, 1));
-				Debug::DrawLine(ray.GetPosition(), closestCollision.collidedAt, Vector4(1, 0, 1, 1), 2000);
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		if (Window::GetKeyboard()->KeyPressed(NCL::KeyboardKeys::L)) {
-			if (selectionObject) {
-				if (lockedObject == selectionObject) {
-					lockedObject = nullptr;
-				}
-				else {
-					lockedObject = selectionObject;
-				}
-			}
-		}
-	}
-	else {
-		Debug::Print("Press Q to change to select mode!", Vector2(5, 85));
-	}
-	return false;
-}
-
-/*
-If an object has been clicked, it can be pushed with the right mouse button, by an amount
-determined by the scroll wheel. In the first tutorial this won't do anything, as we haven't
-added linear motion into our physics system. After the second tutorial, objects will move in a straight
-line - after the third, they'll be able to twist under torque aswell.
-*/
-
-void PaintingGame::MoveSelectedObject() {
-	Debug::Print("Click Force:" + std::to_string(forceMagnitude), Vector2(5, 90));
-	forceMagnitude += Window::GetMouse()->GetWheelMovement() * 100.0f;
-
-	if (!selectionObject) {
-		return;//we haven't selected anything!
-	}
-	//Push the selected object!
-	if (Window::GetMouse()->ButtonPressed(NCL::MouseButtons::RIGHT)) {
-		Ray ray = CollisionDetection::BuildRayFromMouse(*world->GetMainCamera());
-
-		RayCollision closestCollision;
-		if (world->Raycast(ray, closestCollision, true, NULL)) {
-			if (closestCollision.node == selectionObject) {
-				selectionObject->GetPhysicsObject()->AddForceAtPosition(ray.GetDirection() * forceMagnitude, closestCollision.collidedAt);
-			}
-		}
-	}
 }
 
 
--- PaintingGame/PaintingGame.h
+++ PaintingGame/PaintingGame.h
@@ -20,35 +20,11 @@ namespace NCL {
 		public:
 			PaintingGame();
 			~PaintingGame();
-
 			virtual void UpdateGame(float dt);
-
 		protected:
 			void InitialiseAssets();
-
 			void InitCamera();
-
 			void InitWorld();
-
-			/*
-			These are some of the world/object creation functions I created when testing the functionality
-			in the module. Feel free to mess around with them to see different objects being created in different
-			test scenarios (constraints, collision types, and so on).
-			*/
-			void InitGameExamples();
-
-			void InitSphereGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, float radius);
-			void InitMixedGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing);
-			void InitCubeGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, const Vector3& cubeDims);
-
-			void InitDefaultFloor();
-
-			bool SelectObject();
-			void MoveSelectedObject();
-
-			GameObject* AddFloorToWorld(const Vector3& position);
-			GameObject* AddSphereToWorld(const Vector3& position, float radius, float inverseMass = 10.0f);
-			GameObject* AddCubeToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f);
 
 #ifdef USEVULKAN
 			GameTechVulkanRenderer* renderer;
@@ -60,11 +36,8 @@ namespace NCL {
 
 			bool useGravity = true;
 			bool useFog = true;
-			bool inSelectionMode;
 
 			float		forceMagnitude;
-
-			GameObject* selectionObject = nullptr;
 
 			std::map<std::string, MeshGeometry*> meshes;
 			std::map<std::string, MeshMaterial*> meshMaterials;
@@ -72,17 +45,8 @@ namespace NCL {
 
 			std::map<std::string, TextureBase*> textures;
 			std::map<std::string, ShaderBase*> shaders;
-			NavigationGrid* hedgeMaze;
 
 			//Coursework Additional functionality	
-			GameObject* lockedObject = nullptr;
-			Vector3 lockedOffset = Vector3(0, 14, 20);
-			void LockCameraToObject(GameObject* o) {
-				lockedObject = o;
-			}
-
-			GameObject* objClosest = nullptr;
-			
 			float remainingTime = 30;
 
 		};
