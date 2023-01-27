06a90098335a52cb21ce30ba1aef4fdca8f57d19
Aiman Khan <c2055955@newcastle.ac.uk>
2023-01-27 17:06:59 +0000

    resolved release mode issues with react physics 3d

--- ReactPhysics3D/lib/cmake/ReactPhysics3D/ReactPhysics3DConfig-release.cmake
+++ ReactPhysics3D/lib/cmake/ReactPhysics3D/ReactPhysics3DConfig-release.cmake
@@ -9,11 +9,11 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 set_property(TARGET ReactPhysics3D::ReactPhysics3D APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(ReactPhysics3D::ReactPhysics3D PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/reactphysics3d.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/Release/reactphysics3d.lib"
   )
 
 list(APPEND _cmake_import_check_targets ReactPhysics3D::ReactPhysics3D )
-list(APPEND _cmake_import_check_files_for_ReactPhysics3D::ReactPhysics3D "${_IMPORT_PREFIX}/lib/reactphysics3d.lib" )
+list(APPEND _cmake_import_check_files_for_ReactPhysics3D::ReactPhysics3D "${_IMPORT_PREFIX}/lib/Release/reactphysics3d.lib" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
