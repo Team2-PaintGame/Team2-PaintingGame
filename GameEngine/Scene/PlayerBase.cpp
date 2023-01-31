56d3581b05b948224f0a37a93f7b5f72aba20d01
Aiman Khan <c2055955@newcastle.ac.uk>
2023-01-31 15:54:44 +0000

    resolved bounding box issue

--- GameEngine/Scene/PlayerBase.cpp
+++ GameEngine/Scene/PlayerBase.cpp
@@ -13,13 +13,12 @@ PlayerBase::PlayerBase(reactphysics3d::PhysicsCommon& physicsCommon, reactphysic
 	renderObject = new RenderObject(&transform, mesh, shader);
 	renderObject->AddTexture(0, texture, "mainTex");
 
-	boundingVolume = physicsCommon.createBoxShape(rp3d::Vector3(size, size, size));
-
-	reactphysics3d::Transform rp3d_transform(rp3d::Vector3(position.x, position.y, position.x), rp3d::Quaternion::identity());
+	boundingVolume = physicsCommon.createBoxShape(rp3d::Vector3(size / 2.0f, size / 2.0f, size / 2.0f));
+	reactphysics3d::Transform rp3d_transform(rp3d::Vector3(position.x, position.y, position.z), rp3d::Quaternion::identity());
 	
 	// Create a rigid body in the physics world
 	collisionBody = physicsWorld->createRigidBody(rp3d_transform);
-	collisionBody->addCollider(boundingVolume, rp3d_transform); //collider
+	collisionBody->addCollider(boundingVolume, rp3d::Transform::identity()); //collider
 	dynamic_cast<rp3d::RigidBody*>(collisionBody)->updateMassPropertiesFromColliders();
 
 	//in case of material
