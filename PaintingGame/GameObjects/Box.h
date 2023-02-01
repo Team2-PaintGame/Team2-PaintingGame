db3226954b0fb5c02b18b376fedf9716a60573c6
Aiman Khan <c2055955@newcastle.ac.uk>
2023-02-01 14:35:34 +0000

    updated all game object files to use util function for vector3 conversion

--- PaintingGame/GameObjects/Box.h
+++ PaintingGame/GameObjects/Box.h
@@ -4,6 +4,7 @@
 #include "TextureBase.h"
 #include "ShaderBase.h"
 #include "RenderObject.h"
+#include "Utils.h"
 
 namespace NCL {
 	using namespace Rendering;
@@ -19,8 +20,8 @@ namespace NCL {
 			renderObject = new RenderObject(&transform, mesh, shader);
 			renderObject->AddTexture(texture);
 
-			boundingVolume = physicsCommon.createBoxShape(rp3d::Vector3(size / 2.0f, size / 2.0f, size / 2.0f));
-			reactphysics3d::Transform rp3d_transform(rp3d::Vector3(position.x, position.y, position.z), rp3d::Quaternion::identity());
+			boundingVolume = physicsCommon.createBoxShape(~transform.GetScale() / 2.0f);
+			reactphysics3d::Transform rp3d_transform(~position, rp3d::Quaternion::identity());
 
 			// Create a rigid body in the physics world
 			rigidBody = physicsWorld->createRigidBody(rp3d_transform);
