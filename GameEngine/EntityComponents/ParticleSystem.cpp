7232936d563d9d5ab75f838f60631d4400736ee1
Aiman Khan <c2055955@newcastle.ac.uk>
2023-02-09 15:41:40 +0000

    cleaned code in particle systems files

--- GameEngine/EntityComponents/ParticleSystem.cpp
+++ GameEngine/EntityComponents/ParticleSystem.cpp
@@ -7,7 +7,7 @@ ParticleSystem::ParticleSystem(reactphysics3d::PhysicsCommon& physicsCommon, rea
 		particles.push_back(new Particle(physicsCommon, physicsWorld, size));
 	}
 
-	
+	std::vector<Transform*> transforms;
 	for (auto& particle : particles) {
 		transforms.push_back(&particle->GetTransform());
 	}
@@ -17,8 +17,41 @@ ParticleSystem::ParticleSystem(reactphysics3d::PhysicsCommon& physicsCommon, rea
 	renderObject->SetInstanceCount(numParticles);
 }
 
+void ParticleSystem::Update(float dt) {
+	for (auto& p : particles) {
+		p->Update(dt);
+	}
+}
+
 ParticleSystem::~ParticleSystem() {
 	for (auto const& particle : particles) {
 		delete particle;
 	}
 }
+
+Particle::Particle(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, int size) : GameObject(physicsCommon, physicsWorld) {
+	Vector3 position = Vector3(rand() % 50, 10, rand() % 50);
+
+	transform
+		.SetPosition(position)
+		.SetScale(Vector3(size));
+
+	boundingVolume = physicsCommon.createBoxShape(~transform.GetScale() / 2.0f);
+	reactphysics3d::Transform rp3d_transform(~position, rp3d::Quaternion::identity());
+
+	rigidBody = physicsWorld->createRigidBody(rp3d_transform);
+	rigidBody->addCollider(boundingVolume, rp3d::Transform::identity()); 
+	rigidBody->updateMassPropertiesFromColliders();
+}
+
+void Particle::Update(float dt) {
+	//need to call this here because particles itself are not part of game world
+	GameObject::UpdateTransform();
+
+	elapsedTime += dt;
+}
+
+Particle::~Particle() {
+	physicsCommon.destroyBoxShape(boundingVolume);
+}
+
--- GameEngine/EntityComponents/ParticleSystem.h
+++ GameEngine/EntityComponents/ParticleSystem.h
@@ -10,52 +10,25 @@ namespace NCL {
 
 	class Particle : public GameObject {
 	public:
-		Particle(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, int size) : GameObject(physicsCommon, physicsWorld) {
-			Vector3 position = Vector3(rand() % 50, 10, rand() % 50);
-
-			transform
-				.SetPosition(position)
-				.SetScale(Vector3(size));
-			
-			boundingVolume = physicsCommon.createBoxShape(~transform.GetScale() / 2.0f);
-			reactphysics3d::Transform rp3d_transform(~position, rp3d::Quaternion::identity());
-
-			rigidBody = physicsWorld->createRigidBody(rp3d_transform);
-			rigidBody->addCollider(boundingVolume, rp3d::Transform::identity()); //collider
-			rigidBody->updateMassPropertiesFromColliders();
-		}
-
-		void Update(float dt) {
-			//need to call this here because particles itself are not part of game world
-			GameObject::UpdateTransform();
-
-			elapsedTime += dt;
-		}
-
-		virtual ~Particle() {
-			physicsCommon.destroyBoxShape(boundingVolume);
-		}
+		Particle(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, int size);
+		void Update(float dt);
+		virtual ~Particle();
+	
 	protected:
 		Vector3 velocity;
 		float lifeSpan = 3.0f;
 		float elapsedTime = 0.0f;
 		rp3d::BoxShape* boundingVolume;
 	};
+	
 	class ParticleSystem : public GameObject {
 	public:
 		ParticleSystem(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, MeshGeometry* mesh, TextureBase* texture, ShaderBase* shader, int size = 1, std::string name = "Particle System");
-		
-		void Update(float dt) {
-			for (auto& p : particles) {
-				p->Update(dt);
-			}
-		}
-		
+		void Update(float dt);		
 		virtual ~ParticleSystem();
 		
 	protected:
 		unsigned int numParticles = 100;
 		std::vector<Particle*> particles;
-		std::vector<Transform*> transforms;
 	};
 }
