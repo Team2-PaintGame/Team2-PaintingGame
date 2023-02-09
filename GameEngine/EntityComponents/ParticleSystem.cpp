4e4834b319a361760a9931813fe24dfe9e89117e
Aiman Khan <c2055955@newcastle.ac.uk>
2023-02-09 13:12:42 +0000

    updated particle system files

--- GameEngine/EntityComponents/ParticleSystem.cpp
+++ GameEngine/EntityComponents/ParticleSystem.cpp
@@ -1,1 +1,18 @@
 #include "ParticleSystem.h"
+
+using namespace NCL;
+
+ParticleSystem::ParticleSystem(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, MeshGeometry* mesh, TextureBase* texture, ShaderBase* shader, int size, std::string name) : GameObject(physicsCommon, physicsWorld, name) {
+	for (int i = 0; i < numParticles; i++) {
+		particles.push_back(Particle(size));
+	}
+
+	std::vector<Transform*> transforms;
+	for (auto& particle : particles) {
+		transforms.push_back(&particle.GetTransform());
+	}
+
+	renderObject = new RenderObject(transforms, mesh, shader);
+	renderObject->AddTexture(texture);
+	mesh->SetInstanceCount(numParticles);
+}
--- GameEngine/EntityComponents/ParticleSystem.h
+++ GameEngine/EntityComponents/ParticleSystem.h
@@ -1,25 +1,51 @@
 #pragma once
 #include "Vector3.h"
 #include "GameObject.h"
+#include "RenderObject.h"
+#include "Utils.h"
+#include "Matrix4.h"
 
 namespace NCL {
 	using namespace Maths;
 	using namespace CSC8508;
 
 	class Particle {
-		Vector3 position;
+	public:
+		Particle(int size) {
+			Vector3 position = Vector3(rand() % 50, 10, rand() % 50);
+
+			transform
+				.SetPosition(position)
+				.SetScale(Vector3(size));
+		}
+
+		void Update(float dt) {
+			elapsedTime += dt;
+		}
+		Transform& GetTransform() { return transform; }
+	protected:
+		Transform transform;
 		Vector3 velocity;
+		float lifeSpan = 3.0f;
+		float elapsedTime = 0.0f;
 	};
 	class ParticleSystem : public GameObject {
 	public:
+		ParticleSystem(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, MeshGeometry* mesh, TextureBase* texture, ShaderBase* shader, int size = 1, std::string name = "Particle System");
 		
-		//this will have meshgeometry
-
-		//sending to shader
-		//one particle system
-		//binds the mesh
-		//
+		void Update(float dt) {
+			for (auto& p : particles) {
+				p.Update(dt);
+			}
+		}
+		
+		virtual ~ParticleSystem() {
+			//physicsCommon.destroyBoxShape(boundingVolume);
+		}
+		
 	protected:
-		
+		unsigned int numParticles = 100;
+		std::vector<Particle> particles;
+		rp3d::BoxShape* boundingVolume;
 	};
 }
