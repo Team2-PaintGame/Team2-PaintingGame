ff7344e65baaad18503b8f9b7984fe4ffbbe25e3
Aiman Khan <c2055955@newcastle.ac.uk>
2023-02-09 15:38:01 +0000

    Made particle a gameObject

--- GameEngine/EntityComponents/ParticleSystem.cpp
+++ GameEngine/EntityComponents/ParticleSystem.cpp
@@ -4,15 +4,21 @@ using namespace NCL;
 
 ParticleSystem::ParticleSystem(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, MeshGeometry* mesh, TextureBase* texture, ShaderBase* shader, int size, std::string name) : GameObject(physicsCommon, physicsWorld, name) {
 	for (int i = 0; i < numParticles; i++) {
-		particles.push_back(Particle(size));
+		particles.push_back(new Particle(physicsCommon, physicsWorld, size));
 	}
 
-	std::vector<Transform*> transforms;
+	
 	for (auto& particle : particles) {
-		transforms.push_back(&particle.GetTransform());
+		transforms.push_back(&particle->GetTransform());
 	}
 
 	renderObject = new RenderObject(transforms, mesh, shader);
 	renderObject->AddTexture(texture);
 	renderObject->SetInstanceCount(numParticles);
+}
+
+ParticleSystem::~ParticleSystem() {
+	for (auto const& particle : particles) {
+		delete particle;
+	}
 }
--- GameEngine/EntityComponents/ParticleSystem.h
+++ GameEngine/EntityComponents/ParticleSystem.h
@@ -3,31 +3,43 @@
 #include "GameObject.h"
 #include "RenderObject.h"
 #include "Utils.h"
-#include "Matrix4.h"
 
 namespace NCL {
 	using namespace Maths;
 	using namespace CSC8508;
 
-	class Particle {
+	class Particle : public GameObject {
 	public:
-		Particle(int size) {
+		Particle(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, int size) : GameObject(physicsCommon, physicsWorld) {
 			Vector3 position = Vector3(rand() % 50, 10, rand() % 50);
 
 			transform
 				.SetPosition(position)
 				.SetScale(Vector3(size));
+			
+			boundingVolume = physicsCommon.createBoxShape(~transform.GetScale() / 2.0f);
+			reactphysics3d::Transform rp3d_transform(~position, rp3d::Quaternion::identity());
+
+			rigidBody = physicsWorld->createRigidBody(rp3d_transform);
+			rigidBody->addCollider(boundingVolume, rp3d::Transform::identity()); //collider
+			rigidBody->updateMassPropertiesFromColliders();
 		}
 
 		void Update(float dt) {
+			//need to call this here because particles itself are not part of game world
+			GameObject::UpdateTransform();
+
 			elapsedTime += dt;
 		}
-		Transform& GetTransform() { return transform; }
+
+		virtual ~Particle() {
+			physicsCommon.destroyBoxShape(boundingVolume);
+		}
 	protected:
-		Transform transform;
 		Vector3 velocity;
 		float lifeSpan = 3.0f;
 		float elapsedTime = 0.0f;
+		rp3d::BoxShape* boundingVolume;
 	};
 	class ParticleSystem : public GameObject {
 	public:
@@ -35,17 +47,15 @@ namespace NCL {
 		
 		void Update(float dt) {
 			for (auto& p : particles) {
-				p.Update(dt);
+				p->Update(dt);
 			}
 		}
 		
-		virtual ~ParticleSystem() {
-			//physicsCommon.destroyBoxShape(boundingVolume);
-		}
+		virtual ~ParticleSystem();
 		
 	protected:
 		unsigned int numParticles = 100;
-		std::vector<Particle> particles;
-		rp3d::BoxShape* boundingVolume;
+		std::vector<Particle*> particles;
+		std::vector<Transform*> transforms;
 	};
 }
