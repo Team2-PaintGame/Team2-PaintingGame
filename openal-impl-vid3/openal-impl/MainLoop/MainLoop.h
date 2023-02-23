5a801e4bac715a012413f6ddfd5104091a4cecd6
MRZWTtttt <84973035+MRZWTtttt@users.noreply.github.com>
2023-02-23 22:15:30 +0000

    Add files via upload

---
+++ openal-impl-vid3/openal-impl/MainLoop/MainLoop.h
@@ -0,0 +1,212 @@
+/*
+MainLoop.h
+----------------------------------------------------------------------
+
+/* Usage:
+  1. Populate the run functions with lambdas i.e. MainLoop::Get()->AddToOnUpdate([](){std::cout << "update!\n"; };
+  2. Start MainLoop::Run()
+  3. Watch your simulation go.
+*/
+
+#pragma once
+#include <functional>
+#include <memory>
+class MainLoop
+{
+public:
+	static std::shared_ptr<MainLoop> Init();
+	static std::shared_ptr<MainLoop> Get();
+	bool IsLive();
+	void SetDelayLength(const float &delay);
+	void SetRunCondition(bool (*function)());
+	void AddToOnBegin(void (*function)());
+	void AddToOnUpdate(void (*function)(float));
+	void AddToOnUpdate(void (*function)());
+	void AddToDelayedUpdate(void (*function)(float));
+	void AddToDelayedUpdate(void (*function)());
+	void AddToOnPostUpdate(void (*function)());
+	void AddToOnTeardown(void (*function)());
+	int Run();
+
+private:
+	float p_delay_length = .1f;
+	bool p_CheckRunCondition();
+	void p_Begin();
+	void p_Update();
+	void p_Render();
+	void p_Teardown();
+};
+
+// __--****--__ IMPLEMENTATION __--****--__
+
+#include <vector>
+#include <chrono>
+
+static std::shared_ptr<MainLoop> __MainLoop__ = NULL;
+
+std::shared_ptr<MainLoop> MainLoop::Init()
+{
+	if (__MainLoop__ == NULL)
+	{
+		__MainLoop__ = std::make_shared<MainLoop>();
+	}
+	return __MainLoop__;
+}
+
+std::shared_ptr<MainLoop> MainLoop::Get()
+{
+	return MainLoop::Init();
+}
+
+bool MainLoop::IsLive()
+{
+	return __MainLoop__ != NULL;
+}
+
+static std::function<bool()> onContinueCheck;
+static std::vector<std::function<void()>> onBegin;
+static std::vector<std::function<void(float)>> onDeltaUpdate;
+static std::vector<std::function<void()>> onUpdate;
+static std::vector<std::function<void(float)>> onDelayedDeltaUpdate;
+static std::vector<std::function<void()>> onDelayedUpdate;
+static std::vector<std::function<void()>> onRender;
+static std::vector<std::function<void()>> onTeardown;
+
+void MainLoop::SetDelayLength(const float &delay)
+{
+	p_delay_length = delay;
+}
+
+void MainLoop::SetRunCondition(bool (*function)())
+{
+	onContinueCheck = function;
+}
+
+void MainLoop::AddToOnBegin(void (*function)())
+{
+	onBegin.push_back(function);
+}
+
+void MainLoop::AddToOnUpdate(void (*function)(float))
+{
+	onDeltaUpdate.push_back(function);
+}
+
+void MainLoop::AddToOnUpdate(void (*function)())
+{
+	onUpdate.push_back(function);
+}
+
+void MainLoop::AddToDelayedUpdate(void (*function)(float))
+{
+	onDelayedDeltaUpdate.push_back(function);
+}
+
+void MainLoop::AddToDelayedUpdate(void (*function)())
+{
+	onDelayedUpdate.push_back(function);
+}
+
+void MainLoop::AddToOnPostUpdate(void (*function)())
+{
+	onRender.push_back(function);
+}
+
+void MainLoop::AddToOnTeardown(void (*function)())
+{
+	onTeardown.push_back(function);
+}
+
+int MainLoop::Run()
+{
+	if (__MainLoop__ == NULL)
+		MainLoop::Init();
+	p_Begin();
+	while (p_CheckRunCondition())
+	{
+		p_Update();
+		p_Render();
+	}
+	p_Teardown();
+	return 0;
+}
+
+bool MainLoop::p_CheckRunCondition()
+{
+	if (onContinueCheck == NULL)
+		return false;
+	else
+		return onContinueCheck();
+}
+
+void MainLoop::p_Begin()
+{
+	for (const auto &oB : onBegin)
+	{
+		oB();
+	}
+}
+
+void MainLoop::p_Update()
+{
+
+	for (const auto &oU : onUpdate)
+	{
+		oU();
+	}
+
+	static std::chrono::system_clock::time_point currTime;
+	static std::chrono::system_clock::time_point lastTime = std::chrono::system_clock::now();
+	static std::chrono::duration<float> deltaTime;
+
+	currTime = std::chrono::system_clock::now();
+	deltaTime = currTime - lastTime;
+	lastTime = currTime;
+
+	float elapsedTime = deltaTime.count();
+	for (const auto &oDU : onDeltaUpdate)
+	{
+		oDU(elapsedTime);
+	}
+
+	static float delayedUpdateTimeoutSoFar = 0.f;
+	delayedUpdateTimeoutSoFar += elapsedTime;
+	if (delayedUpdateTimeoutSoFar > p_delay_length)
+	{
+		for (auto &odU : onDelayedUpdate)
+		{
+			odU();
+		}
+		for (auto &odDU : onDelayedDeltaUpdate)
+		{
+			odDU(delayedUpdateTimeoutSoFar); //combines all missed delta time since last update
+		}
+		delayedUpdateTimeoutSoFar = 0.f; // reset
+	}
+}
+
+void MainLoop::p_Render()
+{
+	for (const auto &oR : onRender)
+	{
+		oR();
+	}
+}
+
+void MainLoop::p_Teardown()
+{
+	for (const auto &oT : onTeardown)
+	{
+		oT();
+	}
+
+	onBegin.clear();
+	onDeltaUpdate.clear();
+	onUpdate.clear();
+	onDelayedDeltaUpdate.clear();
+	onDelayedUpdate.clear();
+	onRender.clear();
+	onTeardown.clear();
+
+	__MainLoop__ = NULL;
+}
---
+++ openal-impl-vid3/openal-impl/SoundDevice.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <AL\alc.h>
+
+/// <summary>
+/// Singleton class that initilizes our sound hardware device and allows
+/// us to change our listener values.
+/// </summary>
+
+#define SD_INIT SoundDevice::Init();
+#define LISTENER SoundDevice::Get()
+
+class SoundDevice
+{
+public:
+	static SoundDevice* Get();
+	static void Init();
+
+	void GetLocation(float &x, float& y, float& z);
+	void GetOrientation(float &ori);
+	float GetGain();
+
+	void SetLocation(const float& x, const float& y, const float& z);
+	void SetOrientation(
+		const float& atx, const float& aty, const float& atz,
+		const float& upx, const float& upy, const float& upz);
+	void SetGain(const float& val);
+
+private:
+	SoundDevice();
+	~SoundDevice();
+
+	ALCdevice* p_ALCDevice;
+	ALCcontext* p_ALCContext;
+
+};
+
