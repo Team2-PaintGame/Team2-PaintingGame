e3931f300fd12fc2781825585e755afa5b8c91f7
Aiman Khan <c2055955@newcastle.ac.uk>
2023-02-28 19:34:51 +0000

    added factory variable for asset loader

--- PaintingGame/PlatformConfigurations.h
+++ PaintingGame/PlatformConfigurations.h
@@ -1,5 +1,8 @@
 #pragma once
-#include "PaintingGame.h"
+#include <RendererBase.h>
+#include <AssetLoader.h>
+#include <OGLAssetLoader.h>
+#include <GNMAssetLoader.h>
 
 namespace NCL {
 	namespace CSC8508 {
@@ -9,15 +12,19 @@ namespace NCL {
 				// Determine which platform the user is on
 #ifdef _WIN32
 				rendererFactory = new OGLRendererFactory();
+				assetLoaderFactory = new Assets::OGLAssetLoaderFactory();
 #endif
 #ifdef __ORBIS__
 				rendererFactory = new GNMRendererFactory();
+				assetLoaderFactory = new Assets::GNMAssetLoaderFactory();
 #endif
 			}
 			~PlatformConfigurations() {
 				delete rendererFactory;
+				delete assetLoaderFactory;
 			}
 			RendererFactory* rendererFactory;
+			Assets::AssetLoaderFactory* assetLoaderFactory;
 		};
 	}
 }
