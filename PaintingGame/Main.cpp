74939006120bdc3f9dcf16a8b8b7965e4e25de20
Aiman Khan <c2055955@newcastle.ac.uk>
2023-03-01 19:17:20 +0000

    added while loop in main

--- PaintingGame/Main.cpp
+++ PaintingGame/Main.cpp
@@ -12,7 +12,7 @@ using namespace CSC8508;
 
 
 int main() {
-	Window* w = Window::CreateGameWindow("CSC8508 Game technology!", 1280, 720);
+	Window* w = Window::CreateGameWindow("Painting Game", 1280, 720);
 	GameManager gameManager(w);
 	if (!w->HasInitialised()) {
 		return -1;
@@ -20,8 +20,18 @@ int main() {
 
 	w->ShowOSPointer(true);
 	w->LockMouseToWindow(true);
+	
 	w->GetTimer()->GetTimeDeltaSeconds(); //Clear the timer so we don't get a larget first dt!
 
+	while (w->UpdateWindow() && !Window::GetKeyboard()->KeyDown(KeyboardKeys::ESCAPE)) {
+		float dt = w->GetTimer()->GetTimeDeltaSeconds();
+		if (dt > 0.1f) {
+			std::cout << "Skipping large time delta" << std::endl;
+			continue; //must have hit a breakpoint or something to have a 1 second frame time!
+		}
+		w->SetTitle("Painting Game Frame time: " + std::to_string(1000.0f * dt));
+		//gameManager.Run(w);
+	}
 
 	Window::DestroyGameWindow();
 }
