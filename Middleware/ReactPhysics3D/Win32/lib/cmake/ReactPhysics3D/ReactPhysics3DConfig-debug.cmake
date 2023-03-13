882e1bb41b80746947d7d9985208777bf0357465
Aiman Khan (PGT) <c2055955@newcastle.ac.uk>
2023-03-13 13:24:14 +0000

    added lib files for orbis reactphysics3d and updated cmake

---
+++ Middleware/ReactPhysics3D/Win32/lib/cmake/ReactPhysics3D/ReactPhysics3DConfig-debug.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "ReactPhysics3D::ReactPhysics3D" for configuration "Debug"
+set_property(TARGET ReactPhysics3D::ReactPhysics3D APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(ReactPhysics3D::ReactPhysics3D PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/Debug/reactphysics3d.lib"
+  )
+
+list(APPEND _cmake_import_check_targets ReactPhysics3D::ReactPhysics3D )
+list(APPEND _cmake_import_check_files_for_ReactPhysics3D::ReactPhysics3D "${_IMPORT_PREFIX}/lib/Debug/reactphysics3d.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
