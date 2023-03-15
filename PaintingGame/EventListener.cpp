f0b62a8a348cf80bde62493452b24183d4e330be
Dovydas Simoliunas <b8032726@newcastle.ac.uk>
2023-03-15 13:03:39 +0000

    Reduce the emission of particles, make them not hit each other

--- PaintingGame/EventListener.cpp
+++ PaintingGame/EventListener.cpp
@@ -24,20 +24,31 @@ void GameEventListener::onTrigger(const reactphysics3d::OverlapCallback::Callbac
 			continue;
 		}
         
+
 		reactphysics3d::CollisionBody* cb = nullptr;
 		GameObject* obj = nullptr;
+		bool valid = true;
 		if (void* userData = overlapPair.getBody1()->getUserData())
 		{
 			obj = (GameObject*) userData;
 			cb = overlapPair.getBody1();
+			valid &= obj->GetLayer() == Layer::Paint;
+
+			// Check if we're not coliding with paint or player
+			if (void* userData2 = overlapPair.getBody2()->getUserData())
+			{
+				GameObject* obj2 = (GameObject*)userData2;
+				valid &= !(obj2->GetLayer() == Layer::Paint || obj2->GetLayer() == Layer::Player);
+			}
 		}
 		else if (userData = overlapPair.getBody2()->getUserData())
 		{
 			cb = overlapPair.getBody2();
 			obj = (GameObject*)userData;
+			valid &= obj->GetLayer() == Layer::Paint;
 		}
 
-		if (obj && obj->GetLayer() == Layer::Paint)
+		if (obj && valid)
 		{
 			gameWorld->AddPaintedPosition(obj->GetTransform().GetPosition());
 			obj->SetActive(false);
--- PaintingGame/GameObjects/Ink.h
+++ PaintingGame/GameObjects/Ink.h
@@ -7,15 +7,15 @@ namespace NCL {
 	typedef ParticleSystem<SphereParticle> Ink;
 
 	static Ink* CreateInkSplash(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, Vector3 emitterPosition, MeshGeometry* mesh, Vector4 color, ShaderBase* shader) {
-		Ink* inkSplash = new Ink(physicsCommon, physicsWorld, emitterPosition, 45.0f, mesh, color, shader, true, 0.1f, 0.5f, 40.0f);
-		inkSplash->SetParticleEmissionRate(30);
+		Ink* inkSplash = new Ink(physicsCommon, physicsWorld, emitterPosition, 45.0f, mesh, color, shader, true, 0.1f, 0.5f, 50.0f);
+		inkSplash->SetParticleEmissionRate(15);
 		inkSplash->SetRunTime(1.0f);
 		return inkSplash;
 	}
 
 	static Ink* CreateInkStream(reactphysics3d::PhysicsCommon& physicsCommon, reactphysics3d::PhysicsWorld* physicsWorld, Vector3 emitterPosition, MeshGeometry* mesh, Vector4 color, ShaderBase* shader) {
-		Ink* inkStream = new Ink(physicsCommon, physicsWorld, emitterPosition, 5.0f, mesh, color, shader, true, 0.5, 1.0f, 40.0f);
-		inkStream->SetParticleEmissionRate(30);
+		Ink* inkStream = new Ink(physicsCommon, physicsWorld, emitterPosition, 5.0f, mesh, color, shader, true, 0.5, 1.0f, 50.0f);
+		inkStream->SetParticleEmissionRate(15);
 		inkStream->SetRunTime(1.0f);
 		return inkStream;
 	}
