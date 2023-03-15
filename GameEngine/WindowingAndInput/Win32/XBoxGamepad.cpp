3f6914704a7b7c9dddc18cb48e93446306d4c708
Aiman Khan (PGT) <c2055955@newcastle.ac.uk>
2023-03-15 13:22:29 +0000

    added macro if check for xbox gamepad

--- GameEngine/WindowingAndInput/Win32/XBoxGamepad.cpp
+++ GameEngine/WindowingAndInput/Win32/XBoxGamepad.cpp
@@ -1,3 +1,4 @@
+#ifdef _WIN32
 #include "XBoxGamepad.h"
 
 using namespace std;
@@ -81,3 +82,4 @@ bool XBoxGamepad::IsPressed(WORD button)
 {
 	return (state.Gamepad.wButtons & button) != 0;
 }
+#endif
